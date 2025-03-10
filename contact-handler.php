aa0a62107dd65b078d93b47b82bee0a52e4b85d9
Rein Urmet <reinurmet@ut.ee>
2025-03-10 14:12:53 +0200

    contact formile veel öks fix

--- contact-handler.php
+++ contact-handler.php
@@ -1,29 +1,20 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
     $name = strip_tags(trim($_POST["name"]));
     $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
     $message = trim($_POST["message"]);
 
-    // kontrollib et andmed valid
     if (empty($name) || empty($message) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
         http_response_code(400);
         echo "Please fill in the form correctly.";
         exit;
     }
 
-    
     $recipient = "rein.urmet@gmail.com"; 
-
     $subject = "New contact from $name";
-
-    $email_content = "Name: $name\n";
-    $email_content .= "Email: $email\n";
-    $email_content .= "Message:\n$message\n";
-
+    $email_content = "Name: $name\nEmail: $email\nMessage:\n$message\n";
     $email_headers = "From: $name <$email>";
 
-    // Saadab meili
     if (mail($recipient, $subject, $email_content, $email_headers)) {
         http_response_code(200);
         echo "Thank you for contacting me!";
@@ -32,6 +23,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "Something went wrong, please try again.";
     }
 } else {
+    
     http_response_code(403);
     echo "There was a problem with your submission, please try again.";
 }
